e0511a9ba7791b6b58ee1c36fd2da8be2f1ead5c
2ndsprout <ralphlauren5243@gmail.com>
2024-03-07 11:50:47 +0900

    duck exam complete

--- day4/InheritanceExam.java
+++ day4/InheritanceExam.java
@@ -37,11 +37,14 @@ class 청둥오리 extends 오리 {
 class 흰오리 extends 오리 {
 
 }
-class 고무오리 {
+class 고무오리 extends 오리 {
+    // 메소드 오버라이드 (재정의)
+    // 부모한테 물려받은 메소드와 동일한 이름의 메소드를 자식이 다시 정의하는것
+    // 자신에게 가장 가까운 부모 클래스 메소드나, 자신의 메소드를 우선적으로 사용
     public void 날다 () {
         System.out.println("저는 날 수 없어요. ㅠㅠ");
+        // super.날다(); // 부모 클래스의 메소드를 사용하는 법
     }
 }
 class 고무2오리 extends 고무오리 {
-
 }
